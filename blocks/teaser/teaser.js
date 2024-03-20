0039b539a02eca0ac03d148b0e480a1d05fa3789
Kimala Cochran <kimala.cochran@bounteous.com>
2024-03-20 14:39:08 -0600

    adding new teaser block

---
+++ blocks/teaser/teaser.js
@@ -0,0 +1,19 @@
+
+
+export default function decorate(block) {
+    console.log('children', [...block.children]);
+
+    [...block.children].forEach((child) => {
+        console.log('child', child.getElementsByTagName('picture'))
+        if (child.getElementsByTagName('img').length > 0) {
+            console.log('beans');
+            let image = child.getElementsByTagName('img');
+            console.log(image)
+            image[0].className = 'picture44'
+        } else {
+            child.className = 'text123'
+        }
+        
+    })
+
+}
