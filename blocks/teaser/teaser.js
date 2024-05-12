8ea642c3af3e2f29aac2033086ac58d863cff440
Kimala Cochran <kimala.cochran@bounteous.com>
2024-05-11 19:50:13 -0600

    addressing linting issues

--- blocks/teaser/teaser.js
+++ blocks/teaser/teaser.js
@@ -1,15 +1,15 @@
 export default function decorate(block) {
-    console.log('children', [...block.children]);
+  console.log('children', [...block.children]);
 
-    [...block.children].forEach((child) => {
-        console.log('child', child.getElementsByTagName('picture'))
-        if (child.getElementsByTagName('img').length > 0) {
-            console.log('beans');
-            const image = child.getElementsByTagName('img');
-            console.log(image);
-            image[0].className = 'picture44';
-        } else {
-            child.className = 'text123';
-        }
-    })
+  [...block.children].forEach((child) => {
+    console.log('child', child.getElementsByTagName('picture'));
+    if (child.getElementsByTagName('img').length > 0) {
+      console.log('beans');
+      const image = child.getElementsByTagName('img');
+      console.log(image);
+      image[0].className = 'picture44';
+    } else {
+      child.className = 'text123';
+    }
+  });
 }
